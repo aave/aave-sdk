f6a7376f5a22c3f5d2be401792d3d91859f03afa
Cesare Naldi <naldi.cesare@gmail.com>
2025-07-03 20:09:21 +0200

    feat: useEModeToggle, useWithdraw, useRepay, useBorrow, useSupply

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,4 +3,5 @@ export { useAaveClient } from './context';
 export * from './markets';
 export * from './misc';
 export * from './reserves';
+export * from './transactions';
 export * from './user';
--- packages/react/src/transactions.ts
+++ packages/react/src/transactions.ts
@@ -1,6 +1,20 @@
 import type { UnexpectedError } from '@aave/client';
-import type { ExecutionPlan, SupplyRequest } from '@aave/graphql';
-import { supply } from '../../client/src/actions';
+import type {
+  ExecutionPlan,
+  SupplyRequest,
+  BorrowRequest,
+  RepayRequest,
+  WithdrawRequest,
+  EModeToggleRequest,
+  TransactionRequest,
+} from '@aave/graphql';
+import {
+  supply,
+  borrow,
+  repay,
+  withdraw,
+  eModeToggle,
+} from '../../client/src/actions';
 import { useAaveClient } from './context';
 import { type UseAsyncTask, useAsyncTask } from './helpers';
 
@@ -19,4 +33,72 @@ export function useSupply(): UseAsyncTask<
   const client = useAaveClient();
 
   return useAsyncTask((request: SupplyRequest) => supply(client, request));
+}
+
+/**
+ * A hook that provides a way to borrow assets from an Aave market.
+ *
+ * ```ts
+ * const [borrow, { loading, error, data }] = useBorrow();
+ * ```
+ */
+export function useBorrow(): UseAsyncTask<
+  BorrowRequest,
+  ExecutionPlan,
+  UnexpectedError
+> {
+  const client = useAaveClient();
+
+  return useAsyncTask((request: BorrowRequest) => borrow(client, request));
+}
+
+/**
+ * A hook that provides a way to repay borrowed assets to an Aave market.
+ *
+ * ```ts
+ * const [repay, { loading, error, data }] = useRepay();
+ * ```
+ */
+export function useRepay(): UseAsyncTask<
+  RepayRequest,
+  ExecutionPlan,
+  UnexpectedError
+> {
+  const client = useAaveClient();
+
+  return useAsyncTask((request: RepayRequest) => repay(client, request));
+}
+
+/**
+ * A hook that provides a way to withdraw supplied assets from an Aave market.
+ *
+ * ```ts
+ * const [withdraw, { loading, error, data }] = useWithdraw();
+ * ```
+ */
+export function useWithdraw(): UseAsyncTask<
+  WithdrawRequest,
+  ExecutionPlan,
+  UnexpectedError
+> {
+  const client = useAaveClient();
+
+  return useAsyncTask((request: WithdrawRequest) => withdraw(client, request));
+}
+
+/**
+ * A hook that provides a way to toggle eMode for a user in an Aave market.
+ *
+ * ```ts
+ * const [toggleEMode, { loading, error, data }] = useEModeToggle();
+ * ```
+ */
+export function useEModeToggle(): UseAsyncTask<
+  EModeToggleRequest,
+  TransactionRequest,
+  UnexpectedError
+> {
+  const client = useAaveClient();
+
+  return useAsyncTask((request: EModeToggleRequest) => eModeToggle(client, request));
 }
