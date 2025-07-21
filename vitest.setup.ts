16d75565a6508bc22bdc971a791819080e5892a5
Juan Garcia <juan@avara.xyz>
2025-07-21 17:59:35 +0200

    test: separate tests in supply and withdraw

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -4,18 +4,22 @@ import { expect } from 'vitest';
 expect.extend(matchers);
 
 expect.extend({
-  toBeNumericStringCloseTo(received: string, expected: number, precision = 2) {
+  toBeBigDecimalCloseTo(
+    received: string,
+    expected: number | string,
+    precision = 2,
+  ) {
     const numValue = Number(received);
     const pass =
       !Number.isNaN(numValue) &&
-      Math.abs(numValue - expected) < 10 ** -precision;
+      Math.abs(numValue - Number(expected)) < 10 ** -precision;
 
     return {
       pass,
       message: () =>
         pass
           ? `expected "${received}" not to be close to ${expected}`
-          : `expected "${received}" to be close to ${expected}, but got difference of ${Math.abs(numValue - expected)}`,
+          : `expected "${received}" to be close to ${expected}, but got difference of ${Math.abs(numValue - Number(expected))}`,
     };
   },
 });
