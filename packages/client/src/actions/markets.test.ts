b28aaa8867644030399fa0c08c3663c4649130c4
Juan Garcia <juan@avara.xyz>
2025-07-21 11:58:50 +0200

    test: fetch specific market data

--- packages/client/src/actions/markets.test.ts
+++ packages/client/src/actions/markets.test.ts
@@ -1,7 +1,11 @@
 import { assertOk, chainId } from '@aave/types';
 import { describe, expect, it } from 'vitest';
-import { client } from '../test-utils';
-import { markets } from './markets';
+import {
+  client,
+  DEFAULT_MARKET_ADDRESS,
+  ETHEREUM_FORK_ID,
+} from '../test-utils';
+import { market, markets } from './markets';
 
 describe('Given the Aave Protocol v3', () => {
   describe('When fetching markets data', () => {
@@ -20,6 +24,24 @@ describe('Given the Aave Protocol v3', () => {
           supplyReserves: expect.any(Array),
           eModeCategories: expect.any(Array),
         });
+      });
+    });
+  });
+  describe('When fetching a market data for a given address', () => {
+    it('Then it should be possible to fetch market data for a given address', async () => {
+      const result = await market(client, {
+        address: DEFAULT_MARKET_ADDRESS,
+        chainId: ETHEREUM_FORK_ID,
+      });
+
+      assertOk(result);
+
+      expect(result.value).toMatchSnapshot({
+        totalAvailableLiquidity: expect.any(String),
+        totalMarketSize: expect.any(String),
+        borrowReserves: expect.any(Array),
+        supplyReserves: expect.any(Array),
+        eModeCategories: expect.any(Array),
       });
     });
   });
