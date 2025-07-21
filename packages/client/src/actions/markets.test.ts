67c2c1f605a0a53950a7dd1c71deb3f7bab398a2
Juan Garcia <juan@avara.xyz>
2025-07-21 15:04:41 +0200

    test: add fetch vaults and market state

--- packages/client/src/actions/markets.test.ts
+++ packages/client/src/actions/markets.test.ts
@@ -1,11 +1,12 @@
-import { assertOk, chainId } from '@aave/types';
+import { assertOk, chainId, evmAddress } from '@aave/types';
 import { describe, expect, it } from 'vitest';
 import {
   client,
   DEFAULT_MARKET_ADDRESS,
   ETHEREUM_FORK_ID,
+  wallet,
 } from '../test-utils';
-import { market, markets } from './markets';
+import { market, markets, userMarketState } from './markets';
 
 describe('Given the Aave Protocol v3', () => {
   describe('When fetching markets data', () => {
@@ -27,6 +28,7 @@ describe('Given the Aave Protocol v3', () => {
       });
     });
   });
+
   describe('When fetching a market data for a given address', () => {
     it('Then it should be possible to fetch market data for a given market address and chain ID', async () => {
       const result = await market(client, {
@@ -42,6 +44,30 @@ describe('Given the Aave Protocol v3', () => {
         borrowReserves: expect.any(Array),
         supplyReserves: expect.any(Array),
         eModeCategories: expect.any(Array),
+      });
+    });
+  });
+
+  describe('When fetching user market state', () => {
+    it('Then it should be possible to fetch user market state for a given user, market address and chain ID', async () => {
+      const result = await userMarketState(client, {
+        market: DEFAULT_MARKET_ADDRESS,
+        chainId: ETHEREUM_FORK_ID,
+        user: evmAddress(wallet.account!.address),
+      });
+
+      assertOk(result);
+      expect(result.value).toMatchSnapshot({
+        availableBorrowsBase: expect.any(String),
+        currentLiquidationThreshold: expect.any(String),
+        eModeEnabled: expect.any(Boolean),
+        healthFactor: expect.any(String),
+        isInIsolationMode: expect.any(Boolean),
+        ltv: expect.any(String),
+        netAPY: expect.any(String),
+        netWorth: expect.any(String),
+        totalCollateralBase: expect.any(String),
+        totalDebtBase: expect.any(String),
       });
     });
   });
