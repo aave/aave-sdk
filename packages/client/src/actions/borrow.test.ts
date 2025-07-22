23336df94522695903e5609f2cb6817bdf1b6acf
Juan Garcia <juan@avara.xyz>
2025-07-22 13:52:46 +0200

    fix

--- packages/client/src/actions/borrow.test.ts
+++ packages/client/src/actions/borrow.test.ts
@@ -18,7 +18,7 @@ import {
 } from '../test-utils';
 import { sendWith } from '../viem';
 import { market } from './markets';
-import { borrow, supply } from './transactions';
+import { borrow, collateralToggle, supply } from './transactions';
 import { userBorrows, userSupplies } from './user';
 
 async function supplyAndCheck(
@@ -73,50 +73,51 @@ describe('Given an Aave Market', () => {
       evmAddress(wallet.account!.address),
       bigDecimal('0.011'),
     );
-
-    await supplyAndCheck(wallet, {
-      market: marketInfo.address,
-      chainId: marketInfo.chain.chainId,
-      supplier: evmAddress(wallet.account!.address),
-      amount: {
-        erc20: {
-          currency: WETH_ADDRESS,
-          value: '0.01',
-        },
-      },
-    });
   });
 
   describe('And a user with a supply position', () => {
     describe('When user set the supply as collateral', async () => {
       it('Then it should be possible to borrow from the reserve', async () => {
-        // TODO: enable if confirmed that when supplying the isCollateral should be set to false
-        // Enable collateral
-        // const result = await collateralToggle(client, {
-        //   market: initialPosition!.market.address,
-        //   underlyingToken: initialPosition!.currency.address,
-        //   chainId: initialPosition!.market.chain.chainId,
-        //   user: evmAddress(wallet.account!.address),
-        // })
-        //   .andThen(sendWith(wallet))
-        //   .andTee((tx) => console.log(`tx to enable collateral: ${tx}`))
-        //   .andThen(() => {
-        //     return userSupplies(client, {
-        //       markets: [
-        //         {
-        //           address: initialPosition!.market.address,
-        //           chainId: initialPosition!.market.chain.chainId,
-        //         },
-        //       ],
-        //       user: evmAddress(wallet.account!.address),
-        //     });
-        //   });
-        // assertOk(result);
-        // expect(result.value).toEqual([
-        //   expect.objectContaining({
-        //     isCollateral: true,
-        //   }),
-        // ]);
+        const supplyResult = await supplyAndCheck(wallet, {
+          market: marketInfo.address,
+          chainId: marketInfo.chain.chainId,
+          supplier: evmAddress(wallet.account!.address),
+          amount: {
+            erc20: {
+              currency: WETH_ADDRESS,
+              value: '0.01',
+            },
+          },
+        });
+
+        if (!supplyResult[0]!.isCollateral) {
+          // Enable collateral
+          const result = await collateralToggle(client, {
+            market: marketInfo.address,
+            underlyingToken: WETH_ADDRESS,
+            chainId: marketInfo.chain.chainId,
+            user: evmAddress(wallet.account!.address),
+          })
+            .andThen(sendWith(wallet))
+            .andTee((tx) => console.log(`tx to enable collateral: ${tx}`))
+            .andThen(() => {
+              return userSupplies(client, {
+                markets: [
+                  {
+                    address: marketInfo.address,
+                    chainId: marketInfo.chain.chainId,
+                  },
+                ],
+                user: evmAddress(wallet.account!.address),
+              });
+            });
+          assertOk(result);
+          expect(result.value).toEqual([
+            expect.objectContaining({
+              isCollateral: true,
+            }),
+          ]);
+        }
 
         // Borrow from the reserve
         const borrowReserve = await fetchReserve(
@@ -136,7 +137,7 @@ describe('Given an Aave Market', () => {
         })
           .andThen(sendWith(wallet))
           .andTee((tx) => console.log(`tx to borrow: ${tx}`))
-          .andTee(() => wait(10000))
+          .andTee(() => wait(5000))
           .andThen(() =>
             userBorrows(client, {
               markets: [
@@ -149,6 +150,7 @@ describe('Given an Aave Market', () => {
             }),
           );
         assertOk(borrowResult);
+        expect(borrowResult.value.length).toBe(1);
       }, 25_000);
     });
   });
