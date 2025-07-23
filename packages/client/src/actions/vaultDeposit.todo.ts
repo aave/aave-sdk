203515cc934bd70cfb452d1ebcd9b47f0d3135d6
Juan Garcia <juan@avara.xyz>
2025-07-23 16:28:18 +0200

    add todo scenarios for vault

---
+++ packages/client/src/actions/vaultDeposit.todo.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Given the Aave v3 Vaults', () => {
+  describe('When depositing into a vault', () => {
+    it("Then it should be available in the user's vault positions", async () => {
+      expect(true).toBe(true);
+    });
+  });
+});
---
+++ packages/client/src/actions/vaultMint.todo.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Given the Aave v3 Vaults', () => {
+  describe('And a user has deposited assets into a vault', () => {
+    describe('When the user mints shares', () => {
+      it("Then it should be available in the user's vault positions", async () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+});
