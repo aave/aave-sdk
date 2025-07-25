a9ddc47977244836d128a8372dcc8b16a1d8e0b5
Juan Garcia <juan@avara.xyz>
2025-07-25 12:34:55 +0200

    fix: reserve tests

--- vitest.d.ts
+++ vitest.d.ts
@@ -3,5 +3,7 @@ import 'vitest';
 declare module 'vitest' {
   interface AsymmetricMatchersContaining extends JestExtendedMatchers {
     toBeBigDecimalCloseTo: (expected: number | string, precision?: number) => R;
+    toBeWithinBigDecimal: (start: number | string, end: number | string) => R;
+    toBeBetweenDates: (start: Date, end: Date) => R;
   }
 }
