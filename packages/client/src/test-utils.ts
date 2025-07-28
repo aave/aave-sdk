bc1dfadc8a7c72668d07d76ba27c831ce8c66b23
Juan Garcia <juan@avara.xyz>
2025-07-28 11:52:21 +0200

    test: supply/borrow positions

--- packages/client/src/test-utils.ts
+++ packages/client/src/test-utils.ts
@@ -41,7 +41,7 @@ export const WETH_ADDRESS = evmAddress(
   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
 );
 export const USDC_ADDRESS = evmAddress(
-  '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+  '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
 );
 export const DAI_ADDRESS = evmAddress(
   '0x6B175474E89094C44Da98b954EedeAC495271d0F',
@@ -89,6 +89,14 @@ export function createNewWallet(): WalletClient {
   });
   console.log(`wallet address ${wallet.account.address}`);
   return wallet;
+}
+
+export function loadWallet(privateKey: `0x${string}`): WalletClient {
+  return createWalletClient({
+    account: privateKeyToAccount(privateKey),
+    chain: ethereumForkChain,
+    transport: http(),
+  });
 }
 
 // Tenderly RPC type for setBalance
