eca61313547d12367ca9b2101abdfb16488c20b1
Mark Grothe <grothem@gmail.com>
2026-04-22 15:01:36 -0500

    test: skip privy sendtx

--- packages/client/src/privy.test.ts
+++ packages/client/src/privy.test.ts
@@ -23,7 +23,7 @@ const privy = new PrivyClient(
 );
 
 describe('Given a PrivyClient instance', () => {
-  describe('When using it to send Aave v3 transactions', () => {
+  describe.skip('When using it to send Aave v3 transactions', () => {
     it('Then it should work as expected (within current testability constraints)', async () => {
       // Using userSetEmode simply because it's an operation that does not require any specific pre-conditions
       const result = await userSetEmode(client, {
